ade3bea625335dec9b5669d7308119f78ffd3d89
Madison Tindoc <mtindoc@newtech.coppellisd.com>
2022-03-10 14:19:18 -0600

    sdf

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -14,39 +14,94 @@ public class Lab01 {
 	}
 	
 
+	/*As the user for 2 positive integers. The return the first number to 
+	 * the power of the second number as formatted below.
+	 * 
+	 * Enter a positive integer: 3 
+	 * Enter another positive integer: 5 
+	 * 3^5 = 243.0
+	 */
 	
 	public static void problem01() {
+		Scanner inKey = new Scanner(System.in); //activating the scanner
+		int entnum;
+		System.out.println("Put in an positive integer: ");
+		entnum = inKey.nextInt();
+		int entnum1;
+		System.out.println("Enter another in an positive integer: ");
+		entnum1 = inKey.nextInt();
 		
-		
+		double num1Pow = Math.pow(entnum, entnum1);
+		System.out.println(num1Pow + "\n");
 		
 	}
-	
-	
-	
-	
+		
+	/*Ask the user for a positive number (any number will do). Then print 
+	 * that number’s square root as formatted below.
+	 * 
+	 * Enter a positive integer: 30 
+	 * The square root of 16 is 5.477225575051661
+	 */
 	public static void problem02() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		int entnum;
+		System.out.println("Put in an positive integer: ");
+		entnum = inKey.nextInt();
+		double num2 = Math.sqrt(entnum);
+		System.out.println(num2 + "\n");
 		
 	}
 
-	
-	
-
+	/*Ask the user for sides of a right triangle. Then print out the length 
+	 * of the hypotenuse as formatted below.
+	 * 
+	 * Length of side A: 5 
+	 * Length of side B: 7 
+	 * Hypotenuse = 8.602325267042627
+	 */
 	public static void problem03() {
-		
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		double entA, entB, hypotenuse;
+		System.out.println("Length of side A");
+		entA = inKey.nextInt();
+		System.out.println("Length of side B");
+		entB = inKey.nextInt();
+		hypotenuse = Math.sqrt((entA*entA)+(entB*entB));
+		System.out.println("Hypotenuse = " + hypotenuse);
 
 	}
 
-
-	
-	
+	/*Repeatedly ask the user for integers. When they finally enter 0, print 
+	 * out the largest and smallest input.
+	 * 
+	 * Enter an integer: 4 
+	 * Enter an integer: 9 
+	 * Enter an integer: -3 
+	 * Enter an integer: 0 
+	 * Max = 9 
+	 * Min = -3
+	 */
 	public static void problem04() {
-		
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		int entnum;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		System.out.print("Enter an integer: ");
+		entnum = inKey.nextInt();
+		while (entnum > 0)  {
+			System.out.print("Enter an integer: ");
+			entnum = inKey.nextInt();
+			if (entnum < 0)  {
+				System.out.print("Enter an integer: ");
+				entnum = inKey.nextInt();
+				max = Math.max (max, entnum);
+				System.out.println(max);
+				System.out.println("Max = " + max);
+				min = Math.min (min, entnum);
+				System.out.println(min);
+				System.out.println("Min = " + min);
+			}
+		}		
 	}
 	
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -14,40 +14,96 @@ public class Lab02 {
 	}
 	
 
-	
+	/*As the user for 2 positive integers. Then print 10 random numbers 
+	 * between them (inclusive). Note: the user might not put the smaller one first.
+	 * 
+	 * Enter a positive integer: 27 
+	 * Enter another positive integer: 14 
+	 * 17 26 14 18 15 21 27 15 19 19
+	 */
 	public static void problem01() {
-		
-		
-		
-		
+		Scanner inKey = new Scanner(System.in); 
+		double entnum;
+		double entnum2;
+		System.out.println("Put in an positive integer: ");
+		entnum = inKey.nextInt();
+		System.out.println("Put in another positive integer: ");
+		entnum2 = inKey.nextInt();
+		for(int  i = 1;i < 11; i++) {
+			double scope = entnum;
+			double min = entnum2;
+			int num = (int) (Math.random() * scope +  min);
+			System.out.print(num);
+		}
 	}
 	
-	
-	
-	
+	//Ask the user for the radius and height of a cylinder. Then print 
+	//out the cylinder’s volume
 	public static void problem02() {
+		Scanner inKey = new Scanner(System.in);
+		double Radius;
+		double Height;
+		double PI;
+		PI = 3.14159265389793;
+		System.out.println("Enter the radius: ");
+		Radius = inKey.nextInt();
+		System.out.println("Enter the height: ");
+		Height = inKey.nextInt();
 		
-		
-		
+		System.out.println("The radiys is " + PI  * Radius * Radius * Height);
 	}
 
-	
-	
-
+	/*You’ll be replicating the distance formula. Ask the user for the 
+	 * coordinates of 2 points (decimals allowed). Then print the distance between those points.
+	 * 
+	 * Enter x1: 2 
+	 * Enter y1: 3 
+	 * Enter x2: 5 
+	 * Enter y2: 7 
+	 * Distance between points = 5.0
+	 */
 	public static void problem03() {
-
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		double x1;
+		double y1;
+		double x2;
+		double y2;
+		System.out.println("Enter x1: ");
+		x1 = inKey.nextInt();
+		System.out.println("Enter y1: ");
+		y1 = inKey.nextInt();
+		System.out.println("Enter x2: ");
+		x2 = inKey.nextInt();
+		System.out.println("Enter y2: ");
+		y2 = inKey.nextInt();
+		System.out.println("The distance between points");
+		System.out.println( (x2 -  x1) * 2 + (y2 - y1 ) *2 );
 	}
-
-
 	
-	
+	/*You’ll be solving the quadratic formula. Ask the user for the a, b, and c values. Then print 
+	 * BOTH x-intercepts as formatted below. Inputs may be doubles. Note: if the computer prints “NaN” 
+	 * that means the inputs resulted in non-real numbers. Just try with new numbers.
+	 * 
+	 * Enter a-value: 2 
+	 * Enter b-value: -14 
+	 * Enter c-value: 24 
+	 * x1 = 4.0 
+	 * x2 = 3.0
+	 */
 	public static void problem04() {
-		
-		
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		double avalue;
+		double bvalue;
+		double cvalue;
+		double x1;
+		double x2;
+		System.out.println("Enter a-value: ");
+		avalue = inKey.nextInt();
+		System.out.println("Enter b-value: ");
+		bvalue = inKey.nextInt();
+		System.out.println("Enter c-value: ");
+		cvalue = inKey.nextInt();
+		System.out.print("x1 = " + bvalue + "\n x2=" + (bvalue * 2 - 4 * avalue * cvalue));
 	}
 	
 	
